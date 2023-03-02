487810fbacc442f87e48748417943546a708557c
Oliviacrb <oliviacrbates@gmail.com>
2023-03-02 19:09:30 +0000

    html, css and JS added!

---
+++ homepage.js
@@ -0,0 +1,133 @@
+let link1 = document.getElementById("link1");
+let link2 = document.getElementById("link2");
+let link3 = document.getElementById("link3");
+let link4 = document.getElementById("link4");
+let link5 = document.getElementById("link5");
+let link6 = document.getElementById("link6");
+let link7 = document.getElementById("link7");
+let link8 = document.getElementById("link8");
+let text = document.getElementById("text");
+let button = document.getElementById("button");
+
+link1.addEventListener("mouseover", handleMouseOver);
+link1.addEventListener("mouseleave", handleMouseLeave);
+link2.addEventListener("mouseover", handleMouseOver2);
+link2.addEventListener("mouseleave", handleMouseLeave2);
+link3.addEventListener("mouseover", handleMouseOver3);
+link3.addEventListener("mouseleave", handleMouseLeave3);
+link4.addEventListener("mouseover", handleMouseOver4);
+link4.addEventListener("mouseleave", handleMouseLeave4);
+link5.addEventListener("mouseover", handleMouseOver5);
+link5.addEventListener("mouseleave", handleMouseLeave5);
+link6.addEventListener("mouseover", handleMouseOver6);
+link6.addEventListener("mouseleave", handleMouseLeave6);
+link7.addEventListener("mouseover", handleMouseOver7);
+link7.addEventListener("mouseleave", handleMouseLeave7);
+link7.addEventListener("mouseover", handleMouseOver7);
+link7.addEventListener("mouseleave", handleMouseLeave7);
+link8.addEventListener("mouseover", handleMouseOver8);
+link8.addEventListener("mouseleave", handleMouseLeave8);
+text.addEventListener("mouseover", handleMouseOver9);
+text.addEventListener("mouseleave", handleMouseLeave9);
+button.addEventListener("mouseover", handleMouseOver10);
+button.addEventListener("mouseleave", handleMouseLeave10);
+
+function handleMouseOver() {
+   link1.style.color = "grey"
+}
+
+ function handleMouseLeave() {
+    link1.style.color = "white"
+}
+ 
+function handleMouseOver2() {
+    link2.style.color ="grey"
+}
+
+function handleMouseLeave2() {
+    link2.style.color = "white"
+}
+    
+function handleMouseOver3() {
+    link3.style.color = "grey"
+}
+
+function handleMouseLeave3() {
+    link3.style.color = "white"
+}
+
+function handleMouseOver4() {
+    link4.style.color = "grey"
+}
+       
+function handleMouseLeave4() {
+    link4.style.color = "white"
+}
+
+function handleMouseOver5() {
+    link5.style.color = "grey"
+}
+                       
+function handleMouseLeave5() {
+    link5.style.color = "white"
+}
+
+function handleMouseOver6() {
+    link6.style.color = "grey"
+}
+                                                       
+function handleMouseLeave6() {
+    link6.style.color = "white"
+}
+                                                                                                                                                                         
+function handleMouseOver6() {
+    link6.style.color = "grey"
+}
+                                                                                       
+function handleMouseLeave6() {
+    link6.style.color = "white"
+}
+                                                                                                 
+function handleMouseOver7() {
+    link7.style.color = "grey"
+}
+                                                                                                                       
+function handleMouseLeave7() {
+    link7.style.color = "white"
+}
+
+function handleMouseOver8(){
+    link8.style.color = "grey"
+}
+
+function handleMouseLeave8(){
+    link8.style.color = "white"
+}
+
+function handleMouseOver9(){
+    text.style.color = "grey";
+}
+
+function handleMouseLeave9(){
+    text.style.color = "white";
+}
+
+function handleMouseOver10(){
+    button.style.backgroundColor = "green";
+    button.style.color = "white";
+}
+
+function handleMouseLeave10(){
+    button.style.backgroundColor = "white";
+    button.style.color = "black";
+}
+
+
+let = document.getElementById("Button")
+
+button.addEventListener('click', function(){
+    var email = document.getElementById("email").value;
+    alert("Thank You! We'll be in touch!");
+    console.log(email)
+});
+
